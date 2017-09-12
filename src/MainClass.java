7c6a8698bf00a4b65415b26c72ba0cfe8af61f3c
badojo <pdj10211@gmail.com>
2017-09-13 01:28:04 +0900

    기타 : 메인클래스 코드 리셋

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,50 +1,7 @@
-import java.util.Scanner;
-import java.util.Stack;
 
 
 public class MainClass {
     public static void main(String [ ] args) {
-
-        Scanner scan = new Scanner(System.in);
-        int count = scan.nextInt();
-        scan.nextLine();
-
-        String[] strArr = new String[count];
-
-        for(int i=0; i<count; i++) {
-            strArr[i] = scan.nextLine();
-        }
-
-        for(int i=0; i<strArr.length; i++) {
-            if(isVPS(strArr[i])) {
-                System.out.println("YES");
-            }else{
-                System.out.println("NO");
-            }
-        }
-
-    }
-
-    public static boolean isVPS(String str) {
-
-        Stack stack = new Stack();
-        for (Character c : str.toCharArray()) {
-            if(c.compareTo('(') == 0) {
-                stack.add(c);
-            }else{
-
-                if(!stack.isEmpty()) {
-                    stack.pop();
-                }else{
-                    return false;
-                }
-            }
-        }//for() end
-
-        if(!stack.isEmpty()) {
-            return false;
-        }
-
-        return true;
+        //자료구조 테스트
     }
 }
