e244946859bb37de4f5bb470796f4df1ae3671c6
badojo <pdj10211@gmail.com>
2017-09-14 00:15:29 +0900

    자료구조 : 그래프 내부 윤곽 만들어 놓기

---
+++ src/data_structure_graph/Graph.java
@@ -0,0 +1,37 @@
+package data_structure_graph;
+
+/**
+ * Created by parkdongjo on 2017. 9. 13..
+ */
+public class Graph {
+
+    private int[][] matrix = null;
+
+    public Graph(int x, int y) {
+        matrix = new int[x][y];
+    }
+
+    public int insertVertex(int v) {
+        return -1;
+    }
+
+    public int insertEdge(int v1, int v2) {
+        return -1;
+    }
+
+    public int deleteVertex(int v) {
+        return -1;
+    }
+
+    public int deleteEdge(int v1, int v2) {
+        return -1;
+    }
+
+    public boolean IsEmpty() {
+        return true;
+    }
+
+    public void Adjacent(int v) {
+
+    }
+}
