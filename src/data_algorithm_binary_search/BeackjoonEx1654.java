1880b3a38adaec846e8d6253c83a540951f64029
ParkDongJo <pdj10211@gmail.com>
2019-08-16 16:09:09 +0900

    문제풀이 백준 1654 이분탐색

--- src/data_algorithm_binary_search/BeackjoonEx1654.java
+++ src/data_algorithm_binary_search/BeackjoonEx1654.java
@@ -1,8 +1,53 @@
 package data_algorithm_binary_search;
 
+import java.util.Scanner;
+
 public class BeackjoonEx1654 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int k = scan.nextInt();
+        int n = scan.nextInt();
+        int[] nums = new int[k];
+        int sum = 0;
 
+        for (int i=0; i<k; i++) {
+            nums[i] = scan.nextInt();
+            sum += nums[i];
+        }
+
+        System.out.println(solution(nums, sum, n));
 
     }
+
+    public static long solution(int[] arr,int sum, int target) {
+
+        long[] resultArr = new long[sum / target];
+
+        long mid = resultArr[(arr.length)-1];
+        long start = 1;
+        long end = 0;
+
+        while (start >= end) {
+            mid = (start+end)/2;
+
+            long allCount = 0;
+
+            for(int j=0; j<resultArr.length; j++) {
+
+                allCount += resultArr[j]/mid;
+            }
+
+            if(allCount >= target) {
+                end = mid + 1;
+
+            }else if(allCount < target) {
+
+                start = mid - 1;
+
+            }
+        }
+
+        return start;
+    }
+
 }
